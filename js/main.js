7da51dc0953f825c4c8d1752a86af224ed68f573
qqqwwwbbb <usecaut@gmail.com>
2023-01-30 10:00:36 +0700

    css stuff

--- js/main.js
+++ js/main.js
@@ -7,6 +7,9 @@ let app = new Vue({
         altText: "A pair of socks",
         description: "A pair of warm, fuzzy socks",
         image: "./assets/sonic2.jpg",
-        link: "https://www.amazon.com/s?k=sonic&ref=nb_sb_noss"
+        link: "https://www.amazon.com/s?k=sonic&ref=nb_sb_noss",
+        inventory: 5,
+        inStock: true,
+        onSale: true
     }
 })
