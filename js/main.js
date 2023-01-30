93442e1a11af874207d7df4608a938a8defffecc
qqqwwwbbb <usecaut@gmail.com>
2023-01-30 09:19:16 +0700

    prac 1

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        description: "A pair of warm, fuzzy socks"
+        description: "A pair of warm, fuzzy socks",
+        image: "./assets/vmSocks-green-onWhite.jpg"
     }
 })
